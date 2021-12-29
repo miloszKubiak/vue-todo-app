d50bee5e29813fa2038afcecb2e39e88e55033ae
miloszKubiak <milypan69@gmail.com>
2021-12-29 14:42:24 +0100

    add function for adding and functions for unchecking

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div class="container mx-auto">
       <div class="flex items-center text-2xl">
         <router-link to="/" class="inline-block px-10 py-6 transition-colors hover:bg-green-300 duration-300">Home</router-link>
-        <router-link to="/todos" class="inline-block px-10 py-6 transition-colors hover:bg-green-300 duration-300">Todos</router-link>
+        <router-link to="/todos" class="inline-block px-10 py-6 ml-8 transition-colors hover:bg-green-300 duration-300">Todos</router-link>
       </div>
     </div>
   </nav>
--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -4,6 +4,8 @@
       <div class="p-8 bg-green-300 shadow-md rounded w-fit">
         <h2 class="text-xl text-center text-white">Add a task</h2>
         <input
+          v-model="todoText"
+          @keydown.enter="addTodo"
           type="text"
           class="p-2 mt-4 border rounded w-full outline-none"
         />
@@ -16,9 +18,13 @@
 
     <div class="py-4 px-4 mx-2">
       <div
+        v-for="(todo, index) in todos"
+        :key="index"
+        :class="{'bg-green-100': todo.done}"
         class="
           p-8
-          mx-4
+          mx-2
+          my-2
           bg-gray-100
           shadow-md
           rounded
@@ -27,24 +33,36 @@
           justify-between
         "
       >
-        <div>
-          <div>1 Todos</div>
-          <div class="text-gray-500 text-sm">test</div>
+        <div class="container mx-auto">
+          <div>{{ todo.text }}</div>
+          <div class="text-gray-500 text-sm">{{ todo.createdAt.toString() }}
+          </div>
         </div>
-        <div class="space-x-6">
-          <button class="px-2 text-red-400">
+        <div class="flex">
+          <button 
+            class="px-6 text-red-400" 
+            title="Remove todo">
             <i class="fas fa-trash"></i>
           </button>
-          <button class="px-2 text-green-400">
+          <button 
+            v-if="!todo.done"
+            @click="markAsDone(index)" 
+            class="px-2 text-green-400" 
+            title="Mark as done">
             <i class="fas fa-check"></i>
           </button>
-          <button class="px-2 text-yellow-400">
+          <button 
+            v-else
+            @click="markAsUndone(index)"
+            class="px-2 text-yellow-400" 
+            title="Mark as undone">
             <i class="fas fa-undo"></i>
           </button>
         </div>
       </div>
 
       <div
+        v-if="todos.length === 0"
         class="
           px-8
           py-6
@@ -52,9 +70,8 @@
           text-gray-700
           shadow-md
           rounded
-          text-sm
-          mt-5
-          mx-4
+          text-sm text-center
+          m-auto
         "
       >
         You don't have any tasks.
@@ -64,9 +81,38 @@
 </template>
 
 <script>
+import { defineComponent, reactive, ref } from "@vue/runtime-core";
 
-export default {
-  name: "Todos",
-};
+export default defineComponent({
+  setup() {
+    const todos = reactive([]);
+    const todoText = ref("");
 
+    function addTodo() {
+      todos.unshift({
+        text: todoText.value,
+        createdAt: new Date(),
+        done: false,
+      });
+
+      todoText.value = "";
+    }
+
+    function markAsDone(index) {
+      todos[index].done = true;
+    }
+
+    function markAsUndone(index) {
+      todos[index].done = false;
+    }
+
+    return {
+      todos,
+      todoText,
+      addTodo,
+      markAsDone,
+      markAsUndone,
+    };
+  },
+});
 </script>
